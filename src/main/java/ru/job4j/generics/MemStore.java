d8ec6ade9b581faea2c20e1cb4ca86426ab26cb5
Roman  Fetisov <romax777f@gmail.com>
2021-04-07 20:11:30 +0100

    generics

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -16,9 +16,9 @@ public final class MemStore<T extends Base> implements Store<T> {
     public int getIndexById(String id){
         for(int i = 0; i< mem.size();i++) {
             if(Objects.equals(mem.get(i).getId(),id)){
-                int index = mem.indexOf(mem.get(i));
+               // int index = mem.indexOf(mem.get(i));
 
-                return index;
+                return i;
             }
         }return -1;
     }
