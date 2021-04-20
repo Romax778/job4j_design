ce8910e02780eb5ed06c8ffa0c0670e5208f36fe
Roman  Fetisov <romax777f@gmail.com>
2021-04-20 06:02:51 +0100

    collection

--- src/main/java/ru/job4j/collection/SimpleArray.java
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -7,27 +7,31 @@ import java.util.Objects;
 
 public class SimpleArray<T> implements Iterable<T> {
     Object[] container = new Object[3];
-   int modCount;
-public SimpleArray(){
+    int modCount;
 
-}
-    public SimpleArray(int size){
-    container = new Object[size];
+    public SimpleArray() {
+
     }
-int currentIndex = 0;
+
+    public SimpleArray(int size) {
+        container = new Object[size];
+    }
+
+    int currentIndex = 0;
+
     public T get(int index) {
 
-        return (T)container[Objects.checkIndex(index,currentIndex)];
+        return (T) container[Objects.checkIndex(index, currentIndex)];
     }
 
     public void add(T model) {
-        if(container.length == currentIndex){
-            Object[]temp = new Object[container.length * 2];
-            System.arraycopy(container,0,temp,0,container.length);
+        if (container.length == currentIndex) {
+            Object[] temp = new Object[container.length * 2];
+            System.arraycopy(container, 0, temp, 0, container.length);
             container = temp;
         }
-container[currentIndex++] = model;
-modCount++;
+        container[currentIndex++] = model;
+        modCount++;
     }
 
     @Override
@@ -35,33 +39,36 @@ modCount++;
 
 
         return (Iterator<T>) new SimpleArrayIterator();
-}
- class SimpleArrayIterator implements Iterator<Object>{
-int counter = 0;
-int expectedModCount;
-SimpleArrayIterator() {
-    expectedModCount = modCount;
-
-}
-    @Override
-    public boolean hasNext() {
-       if(counter < currentIndex -1) {
-           return true;
-       }
-        return false;
     }
 
-    @Override
-    public Object next() {
-        if (expectedModCount != modCount){
-            throw new ConcurrentModificationException();
+    class SimpleArrayIterator implements Iterator<Object> {
+        int counter = 0;
+        int expectedModCount;
+
+        SimpleArrayIterator() {
+            expectedModCount = modCount;
+
         }
-    if(counter > currentIndex-1){
-        throw new NoSuchElementException();
-    }
-        return container[counter++];
+
+        @Override
+        public boolean hasNext() {
+            if (counter < currentIndex) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return container[counter++];
+
+        }
 
     }
-
-}
 }
